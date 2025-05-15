c99164f424fe6e5e48bda33ae0880c86105b4df9
Alexandr Moroz <log.koyd@gmail.com>
2025-05-15 23:17:21 +0300

    Refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 export default function Header() {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 bg-[rgba(255,255,255,0.995)] font-semibold text-center p-4 sm:p-5 shadow-md select-none">
-      <h1 className="text-2xl sm:text-4xl md:text-5xl">
-        Welcome v etu poebotu
-      </h1>
+      <h1 className="text-2xl sm:text-4xl md:text-5xl">Welcome</h1>
       <h2 className="text-sm sm:text-lg md:text-2xl font-light text-right sm:mr-20 mr-4 mt-2">
         <u className="!mr-20">ISO 27001</u>
       </h2>
