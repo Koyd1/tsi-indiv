e8325bd71a3b5af5fccfb0ec0e29b9b3d938607a
Mysh412 <maia2004best@gmail.com>
2025-05-16 01:16:54 +0300

    header text centered, footer link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import { QRCode_V } from '@/components/index';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex flex-col sm:flex-row items-center justify-center gap-4 py-6 border-t !mt-30">
-      <p className="text-gray-400 text-sm sm:text-base">
-        © 2025 — Что-то придумаем
-      </p>
-      <QRCode_V />
+    <footer className="row-start-3 flex flex-col sm:flex-row sm:justify-between items-center gap-6 py-6 border-t !mt-30 max-w-7xl mx-auto px-4 sm:px-8">
+      <div className="flex flex-col items-center sm:items-start space-y-1 text-gray-400 text-sm sm:text-base">
+        <p>© 2025 — FalcoNS All rights reserved.</p>
+        <p>Адрес: г. Кишинёв, ул. Студенческая, 9/7, блок 3</p>
+        <Link href="/about" className="underline hover:text-gray-600">
+          О проекте
+        </Link>
+      </div>
+      <div>
+        <QRCode_V />
+      </div>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { Typography } from 'antd';
+const { Title, Text } = Typography;
+
 export default function Header() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 bg-[rgba(255,255,255,0.995)] font-semibold text-center p-4 sm:p-5 shadow-md select-none">
-      <h1 className="text-2xl sm:text-4xl md:text-5xl">Welcome</h1>
-      <h2 className="text-sm sm:text-lg md:text-2xl font-light text-right sm:mr-20 mr-4 mt-2">
-        <u className="!mr-20">ISO 27001</u>
-      </h2>
+    <div className="fixed top-0 left-0 right-0 z-50 bg-white/90 backdrop-blur-md shadow-lg px-4 sm:px-8 py-4 sm:py-6 select-none border-b border-gray-200">
+      <div className="max-w-7xl mx-auto text-center">
+        <Title
+          level={3}
+          className="!text-2xl sm:!text-2xl md:!text-3xl !m-0 !font-medium"
+        >
+          Чеклист по внедрению стандартов информационной безопасности
+        </Title>
+        <Text className="block mt-4 text-lg sm:!text-2xl md:!text-2xl font-semibold text-gray-800">
+          <u>ISO 27001</u> & <u>NIST</u>
+        </Text>
+      </div>
     </div>
   );
 }
